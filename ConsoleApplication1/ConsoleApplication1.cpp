a797600db1c5f55e4ded35aceab25f17e0f7d0fb
Rockython <61116928+Rockython@users.noreply.github.com>
2022-09-15 20:34:37 +0700

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+struct Point
+{
+    Point(int64_t X, int64_t Y) : x(X), y(Y) {}
+
+    int64_t x = 0;
+    int64_t y = 0;
+};
+
+int64_t defineDistance()
+{
+    int64_t dist = 0;
+
+    return dist;
+}
+
+int main()
+{
+    std::ifstream fin;
+    std::ofstream fout;
+    size_t amountOfPoints = 0;
+    std::vector<Point> aLotOfPoints;
+    
+    fin.open("input.txt");
+    fin >> amountOfPoints;
+    
+    std::vector<std::vector<double>> graph(amountOfPoints -1, std::vector<double>(amountOfPoints - 1));
+
+    for (size_t i = 0; i < amountOfPoints; ++i)
+    {
+        aLotOfPoints.push_back(Point(0, 0));
+        fin >> aLotOfPoints[i].x >> aLotOfPoints[i].y;
+        fout << aLotOfPoints[i].x << " " << aLotOfPoints[i].y << "\n";
+    }
+    fout.open("output.txt");
+    
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
